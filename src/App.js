76cea2f389df667b4e97bbb504fbeb47e6936587
Colin Orr <colin.orr@gmail.com>
2018-10-14 15:47:36 +0100

    Update the theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -15,38 +17,66 @@ import SpeciesIcon from '@material-ui/icons/BugReport';
 import StarshipsIcon from '@material-ui/icons/AirplanemodeActive';
 import VehiclesIcon from '@material-ui/icons/Motorcycle';
 
-function App() {
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#00A4DB' },
+    secondary: { main: '#15233C' },
+  },
+});
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+  },
+  list: {
+    flex: 1,
+    overflow: 'scroll',
+  },
+  bottomNavigation: {
+    backgroundColor: '#F7F7F7',
+  },
+  bottomNavigationAction: {
+    color: 'red',
+  },
+};
+
+function App(props) {
+  const { classes } = props;
   return (
-    <div style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
-      <AppBar position="relative">
-        <Toolbar>
-          <Typography color="inherit" variant="h6">
-            Star Wars
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <List style={{flex: 1, overflow: 'scroll'}}>
-      {
-        Array(50).fill().map((x, i) => (
-          <ListItem key={i} button>
-            <Avatar><PersonIcon /></Avatar>
-            <ListItemText
-              primary="Person"
-              secondary="Species from Homeworld"
-            />
-          </ListItem>
-        ))
-      }
-      </List>
-      <BottomNavigation value="people">
-        <BottomNavigationAction label="People" value="people" icon={<PeopleIcon />} />
-        <BottomNavigationAction label="Planets" value="planets" icon={<PlanetsIcon />} />
-        <BottomNavigationAction label="Species" value="species" icon={<SpeciesIcon />} />
-        <BottomNavigationAction label="Starships" value="starships" icon={<StarshipsIcon />} />
-        <BottomNavigationAction label="Vehicles" value="vehicles" icon={<VehiclesIcon />} />
-      </BottomNavigation>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <AppBar position="relative" color="secondary">
+          <Toolbar>
+            <Typography color="inherit" variant="h6">
+              Star Wars
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <List className={classes.list}>
+        {
+          Array(50).fill().map((x, i) => (
+            <ListItem key={i} button>
+              <Avatar><PersonIcon /></Avatar>
+              <ListItemText
+                primary="Person"
+                secondary="Species from Homeworld"
+              />
+            </ListItem>
+          ))
+        }
+        </List>
+        <BottomNavigation value="people" className={classes.bottomNavigation}>
+          <BottomNavigationAction label="People" value="people" icon={<PeopleIcon />}  className={classes.bottomNavigationAction} />
+          <BottomNavigationAction label="Planets" value="planets" icon={<PlanetsIcon />} />
+          <BottomNavigationAction label="Species" value="species" icon={<SpeciesIcon />} />
+          <BottomNavigationAction label="Starships" value="starships" icon={<StarshipsIcon />} />
+          <BottomNavigationAction label="Vehicles" value="vehicles" icon={<VehiclesIcon />} />
+        </BottomNavigation>
+      </div>
+    </MuiThemeProvider>
   )
 }
 
-export default App;
+export default withStyles(styles)(App);
