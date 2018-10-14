0c6ab06b2cee0bb803cd6248be4d4ae1ea865f5b
Colin Orr <colin.orr@gmail.com>
2018-10-14 11:26:47 +0000

    Updates

---
+++ precache-manifest.ab19c5271acad3e490886550bf402513.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a6cffc73706e3fa9644",
+    "url": "/starwars-web/static/js/runtime~main.0a6cffc7.js"
+  },
+  {
+    "revision": "5482bee9b77a99b99295",
+    "url": "/starwars-web/static/js/main.5482bee9.chunk.js"
+  },
+  {
+    "revision": "46e007339a06378db462",
+    "url": "/starwars-web/static/js/1.46e00733.chunk.js"
+  },
+  {
+    "revision": "5482bee9b77a99b99295",
+    "url": "/starwars-web/static/css/main.50854054.chunk.css"
+  },
+  {
+    "revision": "03209f38944857851a028acc7529e08a",
+    "url": "/starwars-web/index.html"
+  }
+];
